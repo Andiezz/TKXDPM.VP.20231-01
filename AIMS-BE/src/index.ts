df0b5e77d7d6ea4ede01d82c1d8c40218e854df7
an.vu <an.vu@bnksolution.com>
2023-12-07 16:46:03 +0700

    Feature: split app class to database connection class and web server class

--- AIMS-BE/src/index.ts
+++ AIMS-BE/src/index.ts
@@ -2,6 +2,4 @@ import App from './app'
 import { AuthController } from './business-logic-layer/auth/auth.controller'
 import { UserManagementController } from './business-logic-layer/user-management/index'
 
-const app = new App([new AuthController(), new UserManagementController()])
-
-app.listen()
+new App([new AuthController(), new UserManagementController()])
