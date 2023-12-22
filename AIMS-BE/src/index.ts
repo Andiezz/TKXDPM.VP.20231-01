5598a260f46b43ebf8abf570ca122369e8393e70
NHIMCOII <90151567+NHIMCOII@users.noreply.github.com>
2023-12-22 23:24:58 +0700

    hotfix: resolve conflict

--- AIMS-BE/src/index.ts
+++ AIMS-BE/src/index.ts
@@ -1,15 +1,12 @@
 import App from './app'
 import { AuthController } from './business-logic-layer/auth'
-import { PaymentController } from './business-logic-layer/payment'
-import { UserManagementController } from './business-logic-layer/user-management'
-import { AuthController } from './business-logic-layer/auth/auth.controller'
 import { CdTrackController } from './business-logic-layer/cd-track/cd-track.controller'
 import { DeliveryInfoManagementController } from './business-logic-layer/delivery-info-management'
 import { OrderManagementController } from './business-logic-layer/order-management'
+import { PaymentController } from './business-logic-layer/payment'
 import { ProductController } from './business-logic-layer/product/product.controller'
 import { TrackController } from './business-logic-layer/track'
-import { UserManagementController } from './business-logic-layer/user-management/index'
-import { PaymentController } from './business-logic-layer/payment'
+import { UserManagementController } from './business-logic-layer/user-management'
 
 new App([
     new AuthController(),
