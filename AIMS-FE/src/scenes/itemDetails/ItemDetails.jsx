374efc8d14d5ced9c6c6323635103fa4a8b23b3d
Vũ Thành An <superlegend27@gmail.com>
2023-12-26 18:47:08 +0700

    Feature: Integrate products related apis

--- AIMS-FE/src/scenes/itemDetails/ItemDetails.jsx
+++ AIMS-FE/src/scenes/itemDetails/ItemDetails.jsx
@@ -1,4 +1,16 @@
-import { Box, Button, IconButton, Typography } from '@mui/material';
+import {
+  Box,
+  Button,
+  IconButton,
+  Typography,
+  TableContainer,
+  Table,
+  TableHead,
+  TableCell,
+  TableRow,
+  TableBody,
+  Paper,
+} from '@mui/material';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import { useParams } from 'react-router-dom';
@@ -24,25 +36,26 @@ const ItemDetails = () => {
   };
 
   async function getItem() {
-    const item = await fetch(
-      `http://localhost:2000/api/items/${itemId}?populate=image`,
-      {
-        method: 'GET',
-      }
-    );
+    const item = await fetch(`http://localhost:8080/api/products/${itemId}`, {
+      method: 'GET',
+    });
     const itemJson = await item.json();
     setItem(itemJson.data);
   }
 
   async function getItems() {
     const items = await fetch(
-      `http://localhost:2000/api/items?populate=image`,
+      `http://localhost:8080/api/products?limit=4`,
       {
         method: 'GET',
       }
     );
     const itemsJson = await items.json();
-    setItems(itemsJson.data);
+    setItems(itemsJson.data[0]?.paginatedResults);
+  }
+
+  function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
   }
 
   useEffect(() => {
@@ -56,7 +69,7 @@ const ItemDetails = () => {
         {/* IMAGES */}
         <Box flex="1 1 40%" mb="40px">
           <img
-            alt={item?.name}
+            alt={item?.title}
             width="100%"
             height="100%"
             src={item?.image}
@@ -72,9 +85,248 @@ const ItemDetails = () => {
           </Box>
 
           <Box m="65px 0 25px 0">
-            <Typography variant="h3">{item?.name}</Typography>
+            <Typography variant="h3">{item?.title}</Typography>
             <Typography>${item?.price}</Typography>
-            <Typography sx={{ mt: '20px' }}>{item?.longDescription}</Typography>
+            <Typography>{item?.kind?.toUpperCase()}</Typography>
+            <Typography sx={{ mt: '20px' }}>{item?.description}</Typography>
+            <Typography>
+              {'Import date: ' + new Date(item?.importDate).toLocaleString()}
+            </Typography>
+            <Typography>{'Barcode: ' + item?.barcode}</Typography>
+            <Typography>
+              {'Support rush delivery: '}{' '}
+              {item?.supportRush === true ? 'yes' : 'no'}
+            </Typography>
+            <Typography variant="h5" sx={{ mt: '20px' }}>
+              {item?.kind?.toUpperCase()} Information
+            </Typography>
+            <TableContainer component={Paper}>
+              <Table sx={{ minWidth: 600 }} aria-label="simple table">
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Info</TableCell>
+                    <TableCell align="right">Value</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {item?.kind === 'dvd' && [
+                    <TableRow
+                      key={'discType'}
+                      sx={{
+                        '&:last-child td, &:last-child th': { border: 0 },
+                      }}
+                    >
+                      <TableCell component="th" scope="row">
+                        {capitalizeFirstLetter('discType')}
+                      </TableCell>
+                      <TableCell align="right">{item?.discType}</TableCell>
+                    </TableRow>,
+                    <TableRow
+                      key={'director'}
+                      sx={{
+                        '&:last-child td, &:last-child th': { border: 0 },
+                      }}
+                    >
+                      <TableCell component="th" scope="row">
+                        {capitalizeFirstLetter('director')}
+                      </TableCell>
+                      <TableCell align="right">{item?.director}</TableCell>
+                    </TableRow>,
+                    <TableRow
+                      key={'runtime'}
+                      sx={{
+                        '&:last-child td, &:last-child th': { border: 0 },
+                      }}
+                    >
+                      <TableCell component="th" scope="row">
+                        {capitalizeFirstLetter('runtime')}
+                      </TableCell>
+                      <TableCell align="right">{item?.runtime}</TableCell>
+                    </TableRow>,
+                    <TableRow
+                      key={'studio'}
+                      sx={{
+                        '&:last-child td, &:last-child th': { border: 0 },
+                      }}
+                    >
+                      <TableCell component="th" scope="row">
+                        {capitalizeFirstLetter('studio')}
+                      </TableCell>
+                      <TableCell align="right">{item?.studio}</TableCell>
+                    </TableRow>,
+                    <TableRow
+                      key={'language'}
+                      sx={{
+                        '&:last-child td, &:last-child th': { border: 0 },
+                      }}
+                    >
+                      <TableCell component="th" scope="row">
+                        {capitalizeFirstLetter('language')}
+                      </TableCell>
+                      <TableCell align="right">{item?.language}</TableCell>
+                    </TableRow>,
+                    <TableRow
+                      key={'releaseDate'}
+                      sx={{
+                        '&:last-child td, &:last-child th': { border: 0 },
+                      }}
+                    >
+                      <TableCell component="th" scope="row">
+                        {capitalizeFirstLetter('releaseDate')}
+                      </TableCell>
+                      <TableCell align="right">{new Date(item?.releaseDate).toLocaleString()}</TableCell>
+                    </TableRow>,
+                    <TableRow
+                      key={'genre'}
+                      sx={{
+                        '&:last-child td, &:last-child th': { border: 0 },
+                      }}
+                    >
+                      <TableCell component="th" scope="row">
+                        {capitalizeFirstLetter('genre')}
+                      </TableCell>
+                      <TableCell align="right">{item?.genre}</TableCell>
+                    </TableRow>,
+                    <TableRow
+                      key={'subtitle'}
+                      sx={{
+                        '&:last-child td, &:last-child th': { border: 0 },
+                      }}
+                    >
+                      <TableCell component="th" scope="row">
+                        {capitalizeFirstLetter('subtitle')}
+                      </TableCell>
+                      <TableCell align="right">{item?.subtitle}</TableCell>
+                    </TableRow>,
+                  ]}
+                  {item?.kind === 'book' && [
+                    <TableRow
+                      key={'author'}
+                      sx={{
+                        '&:last-child td, &:last-child th': { border: 0 },
+                      }}
+                    >
+                      <TableCell component="th" scope="row">
+                        {capitalizeFirstLetter('author')}
+                      </TableCell>
+                      <TableCell align="right">{item?.author}</TableCell>
+                    </TableRow>,
+                    <TableRow
+                      key={'coverType'}
+                      sx={{
+                        '&:last-child td, &:last-child th': { border: 0 },
+                      }}
+                    >
+                      <TableCell component="th" scope="row">
+                        {capitalizeFirstLetter('coverType')}
+                      </TableCell>
+                      <TableCell align="right">{item?.coverType}</TableCell>
+                    </TableRow>,
+                    <TableRow
+                      key={'publisher'}
+                      sx={{
+                        '&:last-child td, &:last-child th': { border: 0 },
+                      }}
+                    >
+                      <TableCell component="th" scope="row">
+                        {capitalizeFirstLetter('publisher')}
+                      </TableCell>
+                      <TableCell align="right">{item?.publisher}</TableCell>
+                    </TableRow>,
+                    <TableRow
+                      key={'publicationDate'}
+                      sx={{
+                        '&:last-child td, &:last-child th': { border: 0 },
+                      }}
+                    >
+                      <TableCell component="th" scope="row">
+                        {capitalizeFirstLetter('publicationDate')}
+                      </TableCell>
+                      <TableCell align="right">{new Date(item?.publicationDate).toLocaleString()}</TableCell>
+                    </TableRow>,
+                    <TableRow
+                      key={'language'}
+                      sx={{
+                        '&:last-child td, &:last-child th': { border: 0 },
+                      }}
+                    >
+                      <TableCell component="th" scope="row">
+                        {capitalizeFirstLetter('language')}
+                      </TableCell>
+                      <TableCell align="right">{item?.language}</TableCell>
+                    </TableRow>,
+                    <TableRow
+                      key={'pages'}
+                      sx={{
+                        '&:last-child td, &:last-child th': { border: 0 },
+                      }}
+                    >
+                      <TableCell component="th" scope="row">
+                        {capitalizeFirstLetter('pages')}
+                      </TableCell>
+                      <TableCell align="right">{item?.pages}</TableCell>
+                    </TableRow>,
+                    <TableRow
+                      key={'bookCategory'}
+                      sx={{
+                        '&:last-child td, &:last-child th': { border: 0 },
+                      }}
+                    >
+                      <TableCell component="th" scope="row">
+                        {capitalizeFirstLetter('bookCategory')}
+                      </TableCell>
+                      <TableCell align="right">{item?.bookCategory}</TableCell>
+                    </TableRow>,
+                  ]}
+                  {item?.kind === 'cd' || item?.kind === 'long-play' && [
+                    <TableRow
+                      key={'artist'}
+                      sx={{
+                        '&:last-child td, &:last-child th': { border: 0 },
+                      }}
+                    >
+                      <TableCell component="th" scope="row">
+                        {capitalizeFirstLetter('artist')}
+                      </TableCell>
+                      <TableCell align="right">{item?.artist}</TableCell>
+                    </TableRow>,
+                    <TableRow
+                      key={'recordLabel'}
+                      sx={{
+                        '&:last-child td, &:last-child th': { border: 0 },
+                      }}
+                    >
+                      <TableCell component="th" scope="row">
+                        {capitalizeFirstLetter('recordLabel')}
+                      </TableCell>
+                      <TableCell align="right">{item?.recordLabel}</TableCell>
+                    </TableRow>,
+                    <TableRow
+                      key={'genre'}
+                      sx={{
+                        '&:last-child td, &:last-child th': { border: 0 },
+                      }}
+                    >
+                      <TableCell component="th" scope="row">
+                        {capitalizeFirstLetter('genre')}
+                      </TableCell>
+                      <TableCell align="right">{item?.genre}</TableCell>
+                    </TableRow>,
+                    <TableRow
+                      key={'releaseDate'}
+                      sx={{
+                        '&:last-child td, &:last-child th': { border: 0 },
+                      }}
+                    >
+                      <TableCell component="th" scope="row">
+                        {capitalizeFirstLetter('releaseDate')}
+                      </TableCell>
+                      <TableCell align="right">{new Date(item?.releaseDate)}</TableCell>
+                    </TableRow>,
+                  ]}
+                </TableBody>
+              </Table>
+            </TableContainer>
           </Box>
 
           <Box display="flex" alignItems="center" minHeight="50px">
@@ -105,6 +357,9 @@ const ItemDetails = () => {
             >
               ADD TO CART
             </Button>
+            <Typography sx={{ p: '0 10px' }}>
+              {'In stock: ' + item?.quantity}
+            </Typography>
           </Box>
           <Box>
             <Box m="20px 0 5px 0" display="flex">
@@ -124,7 +379,44 @@ const ItemDetails = () => {
         </Tabs>
       </Box>
       <Box display="flex" flexWrap="wrap" gap="15px">
-        {value === 'description' && <div>{item?.longDescription}</div>}
+        {value === 'description' && (
+          <div>
+            {item?.description}
+            <Typography variant="h5" sx={{ mt: '20px' }}>
+              Product Dimension
+            </Typography>
+            <TableContainer component={Paper}>
+              <Table sx={{ minWidth: 600 }} aria-label="simple table">
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Dimension</TableCell>
+                    <TableCell align="right">Value</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {item?.productDimensions &&
+                    Object.entries(item?.productDimensions)?.map(
+                      ([key, value]) => (
+                        <TableRow
+                          key={key}
+                          sx={{
+                            '&:last-child td, &:last-child th': { border: 0 },
+                          }}
+                        >
+                          <TableCell component="th" scope="row">
+                            {capitalizeFirstLetter(key)}
+                          </TableCell>
+                          <TableCell align="right">
+                            {value !== null ? value : 'N/A'}
+                          </TableCell>
+                        </TableRow>
+                      )
+                    )}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </div>
+        )}
         {value === 'reviews' && <div>reviews</div>}
       </Box>
 
@@ -141,7 +433,7 @@ const ItemDetails = () => {
           justifyContent="space-between"
         >
           {items.slice(0, 4).map((item, i) => (
-            <Item key={`${item.name}-${i}`} item={item} />
+            <Item key={`${item.title}-${i}`} item={item} />
           ))}
         </Box>
       </Box>
